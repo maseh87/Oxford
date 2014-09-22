b828b9836792de99ee52a49e59ccf80d7a9d7a72
Mason <maseh87@gmail.com>
2014-09-22 13:58:21 -0700

    updates

--- src/scripts/main/directives/drag.js
+++ src/scripts/main/directives/drag.js
@@ -13,6 +13,20 @@
     var cards = Draggable.create('.card-material');
     var currentElement;
     var that;
+    function check() {
+      if(this.hitTest(chart, '100') && this.target._gsDragID !== chart[0]._gsDragID) {
+        // console.log(startX, ' y');
+        console.log('Chart hit ', e);
+        console.log('this ', row);
+      }
+      if(this.hitTest(row, '50')) {
+        var mX = row.minX;
+        var mY = row.minY;
+        // TweenLite.to(row, 3, {minX: that.minX, minY: that.minY});
+        this.minY = mY;
+        this.minX = mX;
+      }
+    }
 
     return function(scope, element, attr) {
 
@@ -26,7 +40,7 @@
             startX = this.x;
             startY = this.y;
             that = this;
-            // console.log(this);
+            console.log(angular.element(element));
             // console.log(row);
           },
           snap: {
@@ -38,22 +52,10 @@
             }
           },
           onDrag: function(e) {
-            if(this.hitTest(chart, '100') && this.target._gsDragID !== chart[0]._gsDragID) {
-              // console.log(startX, ' y');
-              console.log('Chart hit ', e);
-              console.log('this ', row);
-            }
-            if(this.hitTest(row, '50')) {
-              var mX = row.minX;
-              var mY = row.minY;
-              // TweenLite.to(row, 3, {minX: that.minX, minY: that.minY});
-              this.minY = mY;
-              this.minX = mX;
-            }
           },
           onDragEnd: function(e) {
-            if(this.hitTest(chart)) {
-              TweenLite.to(e.target, 0.1, {x: 450 , y: 150, ease: Power2.easeInOut});
+            if(this.hitTest(chart, '100')) {
+              TweenLite.to(e.target, 0.1, {x: 0, y: 0, ease: Power2.easeInOut});
               // console.log(this, ' tween');
               // TweenLite.to(element, 0.5, {x: startX, y: startY, ease: Power2.easeInOut});
             }
